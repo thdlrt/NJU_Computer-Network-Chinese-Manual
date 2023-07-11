feb93d4ac7387fc40ba217e5595609d09598499e
thdlrt <1040880153@qq.com>
2023-07-11 22:55:28 +0800

    master

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+nju cs计网中文手册
+
+> designed by Thdlrt with chatgpt
---
+++ book.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // 书籍信息
+  title: 'nju计网手册',
+  description: '描述',
+  isbn: 'xxxx',
+  author: 'thdlrt',
+  lang: 'zh-cn',
+
+  // 插件列表
+  plugins: ["theme-comscore","back-to-top-button","code","pageview-count","search-pro"
+
+],
+
+  // 插件全局配置
+  pluginsConfig: {},
+
+  // 模板变量
+  variables: {
+    // 自定义
+  },
+};
